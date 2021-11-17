8815112ea4ae1209e8ba64a898d5850a073f4fd9
Bryan C Guner <bryan.guner@gmail.com>
2021-11-17 14:31:29 +0000

    up

--- iframe-script.js
+++ iframe-script.js
@@ -1,88 +1,81 @@
 $(document).ready(function () {
-    $(".iframe").responsiveIframes({
-        openMessage: "Full screen",
-        closeMessage: "Close full screen"
-    });
+  $(".iframe").responsiveIframes({
+    openMessage: "Full screen",
+    closeMessage: "Close full screen",
+  });
 });
 
-
 (function ($) {
-    $.responsiveIframes = function (el, options) {
-        var self = this;
+  $.responsiveIframes = function (el, options) {
+    var self = this;
 
-        // Access to jQuery and DOM versions of element
-        self.$el = $(el);
-        self.el = el;
+    // Access to jQuery and DOM versions of element
+    self.$el = $(el);
+    self.el = el;
 
-        // Add a reverse reference to the DOM object
-        self.$el.data("responsiveIframes", self);
+    // Add a reverse reference to the DOM object
+    self.$el.data("responsiveIframes", self);
 
-        self.init = function () {
-            self.options = $.extend(
-                {},
-                $.responsiveIframes.defaultOptions,
-                options
-            );
+    self.init = function () {
+      self.options = $.extend({}, $.responsiveIframes.defaultOptions, options);
 
-            // wrap iframe
-            var iframeSrc = self.$el
-                .find("iframe")
-                .wrap('<div class="iframe-content" />')
-                .attr("src");
+      // wrap iframe
+      var iframeSrc = self.$el
+        .find("iframe")
+        .wrap('<div class="iframe-content" />')
+        .attr("src");
 
-            //generate header
-            var header =
-                '<div class="iframe-header">' +
-                '<a href="' +
-                iframeSrc +
-                '" class="iframe-trigger">' +
-                self.options.openMessage +
-                "</a>" +
-                "</div>";
+      //generate header
+      var header =
+        '<div class="iframe-header">' +
+        '<a href="' +
+        iframeSrc +
+        '" class="iframe-trigger">' +
+        self.options.openMessage +
+        "</a>" +
+        "</div>";
 
-            var trigger = self.$el.prepend(header).find(".iframe-trigger");
+      var trigger = self.$el.prepend(header).find(".iframe-trigger");
 
-            // click event
-            $(trigger).click(function (e) {
-                e.preventDefault();
+      // click event
+      $(trigger).click(function (e) {
+        e.preventDefault();
 
-                var $this = $(this),
-                    $html = $("html"),
-                    isFullScreen = $html.hasClass("iframe-full-screen"),
-                    message = isFullScreen
-                        ? self.options.openMessage
-                        : self.options.closeMessage;
+        var $this = $(this),
+          $html = $("html"),
+          isFullScreen = $html.hasClass("iframe-full-screen"),
+          message = isFullScreen
+            ? self.options.openMessage
+            : self.options.closeMessage;
 
-                $this.text(message);
+        $this.text(message);
 
-                if (isFullScreen) {
-                    self.$el.removeClass("iframe-active");
-                    $html.removeClass("iframe-full-screen");
-                    setTimeout(function () {
-                        $(window).scrollTop(
-                            $this.data("iframe-scroll-position")
-                        );
-                    }, 1);
-                } else {
-                    $this.data("iframe-scroll-position", $(window).scrollTop());
-                    self.$el.addClass("iframe-active");
-                    $html.addClass("iframe-full-screen");
-                }
-            });
-        };
-
-        // Run initializer
-        self.init();
+        if (isFullScreen) {
+          self.$el.removeClass("iframe-active");
+          $html.removeClass("iframe-full-screen");
+          setTimeout(function () {
+            $(window).scrollTop($this.data("iframe-scroll-position"));
+          }, 1);
+        } else {
+          $this.data("iframe-scroll-position", $(window).scrollTop());
+          self.$el.addClass("iframe-active");
+          $html.addClass("iframe-full-screen");
+        }
+      });
     };
 
-    $.responsiveIframes.defaultOptions = {
-        openMessage: "Full screen",
-        closeMessage: "Close"
-    };
+    // Run initializer
+    self.init();
+  };
 
-    $.fn.responsiveIframes = function (options) {
-        return this.each(function () {
-            new $.responsiveIframes(this, options);
-        });
-    };
+  $.responsiveIframes.defaultOptions = {
+    openMessage: "Full screen",
+    closeMessage: "Close",
+  };
+
+  $.fn.responsiveIframes = function (options) {
+    return this.each(function () {
+      new $.responsiveIframes(this, options);
+    });
+  };
 })(jQuery);
